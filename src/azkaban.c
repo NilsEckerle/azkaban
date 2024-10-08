ac728c8156e733846eb0b56ad46c4d5054c9bb1a
nils@oswaldt <nilseckerle2001@gmail.com>
2024-10-09 01:39:53 +0200

    added sdl2 and simple window program

--- src/azkaban.c
+++ src/azkaban.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include "SDL2/SDL.h"
 
 void *callback(void *not_used, Entry entry) {
   printf("id = %d\n", entry.id);
@@ -12,7 +13,7 @@ void *callback(void *not_used, Entry entry) {
   return NULL;
 }
 
-int main(int argc, char *argv[]) {
+int test() {
   printf("You shouldn't run azkaban directly.\nI use it to test some code but "
          "when you can run this, you shouldnt be able to.\nPlease Report this "
          "to your IT Admin.\n");
@@ -118,4 +119,64 @@ int main(int argc, char *argv[]) {
 
   // printf("entries: %d\n", entry_list_get_length(entry_list));
   return 0;
+}
+
+int main(int argc, char *argv[]) {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+		return EXIT_FAILURE;
+	}
+
+	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 620, 387, SDL_WINDOW_SHOWN);
+	if (win == NULL) {
+		fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+		return EXIT_FAILURE;
+	}
+
+	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == NULL) {
+		fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+		if (win != NULL) {
+			SDL_DestroyWindow(win);
+		}
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+
+	SDL_Surface* bmp = SDL_LoadBMP("./img/grumpy-cat.bmp");
+	if (bmp == NULL) {
+		fprintf(stderr, "SDL_LoadBMP Error: %s\n", SDL_GetError());
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
+	if (tex == NULL) {
+		fprintf(stderr, "SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
+		SDL_FreeSurface(bmp);
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
+	SDL_FreeSurface(bmp);
+
+	for (int i = 0; i < 20; i++) {
+		SDL_RenderClear(ren);
+		SDL_RenderCopy(ren, tex, NULL, NULL);
+		SDL_RenderPresent(ren);
+		SDL_Delay(100);
+	}
+
+	SDL_DestroyTexture(tex);
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+
+	return EXIT_SUCCESS;
+
+  // int rc = test();
+  // return rc;
 }
